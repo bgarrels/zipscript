648ef6b14324bf77420f4b52e61efb7278c08b2d
joehud <joehud@caa2032b-004f-0410-ad89-e35968282056>
2008-07-14 00:58:20 +0000

    Issue 4

---
+++ core/src/main/java/hudson/zipscript/parser/exception/UnknownPropertyException.java
@@ -0,0 +1,29 @@
+package hudson.zipscript.parser.exception;
+
+public class UnknownPropertyException extends ExecutionException {
+
+	private String propertyName;
+	private Object caller;
+	
+	public UnknownPropertyException (String propertyName, Object caller) {
+		super(null, null);
+		this.propertyName = propertyName;
+		this.caller = caller;
+	}
+
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+
+	public Object getCaller() {
+		return caller;
+	}
+
+	public void setCaller(Object caller) {
+		this.caller = caller;
+	}
+}
