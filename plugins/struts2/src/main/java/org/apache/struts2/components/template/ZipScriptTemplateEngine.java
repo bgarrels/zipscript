e87d77dbacb91e5e88f03df38ecdad1e384356b5
joehud <joehud@caa2032b-004f-0410-ad89-e35968282056>
2008-08-05 02:11:31 +0000

    added copyright statement

--- plugins/struts2/src/main/java/org/apache/struts2/components/template/ZipScriptTemplateEngine.java
+++ plugins/struts2/src/main/java/org/apache/struts2/components/template/ZipScriptTemplateEngine.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2008 Joe Hudson.  All rights reserved.
+ * License: LGPL <http://www.gnu.org/licenses/lgpl.html>
+ */
+
 package org.apache.struts2.components.template;
 
 import hudson.zipscript.ZipEngine;
