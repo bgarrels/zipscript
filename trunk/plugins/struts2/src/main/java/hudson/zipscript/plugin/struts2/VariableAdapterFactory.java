99f37fb3cce719f92555e198bc3603002458d385
joehud <joehud@caa2032b-004f-0410-ad89-e35968282056>
2008-08-05 02:11:31 +0000

    added copyright statement

--- trunk/plugins/struts2/src/main/java/hudson/zipscript/plugin/struts2/VariableAdapterFactory.java
+++ trunk/plugins/struts2/src/main/java/hudson/zipscript/plugin/struts2/VariableAdapterFactory.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2008 Joe Hudson.  All rights reserved.
+ * License: LGPL <http://www.gnu.org/licenses/lgpl.html>
+ */
+
 package hudson.zipscript.plugin.struts2;
 
 import hudson.zipscript.parser.template.data.ParsingSession;
